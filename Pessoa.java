ab755d1da7092b9b1eeaad8e2f9f3f0eede494ee
anneljsee <annemourag@gmail.com>
2025-10-09 16:32:11 -0300

    Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,99 +1,92 @@
 package classes;
-public class Pessoa {
-    private String nomeCompleto;
-    private String dataNascimento;
-    private Genero genero;
-    private String nacionalidade;
-    private String paisOrigem;
-    private String email;
-    private String telefone;
-    private Idioma idioma;
-    private String senha;
 
+public class pessoa {
+private String nomeCompleto, dataNascimento, gênero, nacionalidade, paisOrigem, email, telefone, senha;
 
-    public Pessoa(String nomeCompleto, String dataNascimento, Genero genero,
-                  String nacionalidade, String paisOrigem, String email,
-                  String telefone, Idioma idioma, String senha) {
-        this.nomeCompleto = nomeCompleto;
-        this.dataNascimento = dataNascimento;
-        this.genero = genero;
-        this.nacionalidade = nacionalidade;
-        this.paisOrigem = paisOrigem;
-        this.email = email;
-        this.telefone = telefone;
-        this.idioma = idioma;
-        this.senha = senha;
-    }
+private Idioma idioma;
+private Genero genero;
+public Pessoa(String nomeCompleto, String dataNascimento, Genero genero, String nacionalidade, String paisOrigem,
+String email, String telefone, Idioma idioma, String senha) {
+this.nomeCompleto = nomeCompleto;
+this.dataNascimento = dataNascimento;
+this.genero = genero;
+this.nacionalidade = nacionalidade;
+this.paisOrigem = paisOrigem;
+this.email = email;
+this.telefone = telefone;
+this.idioma = idioma;
+this.senha = senha;
+}
 
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
+public String getNomeCompleto() {
+return nomeCompleto;
+}
 
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
+public void setNomeCompleto(String nomeCompleto) {
+this.nomeCompleto = nomeCompleto;
+}
 
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
+public String getDataNascimento() {
+return dataNascimento;
+}
 
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
+public void setDataNascimento(String dataNascimento) {
+this.dataNascimento = dataNascimento;
+}
 
-    public Genero getGenero() {
-        return genero;
-    }
+public Genero getGenero() {
+return genero;
+}
 
-    public void setGenero(Genero genero) {
-        this.genero = genero;
-    }
+public void setGenero(Genero genero) {
+this.genero = genero;
+}
 
-    public String getNacionalidade() {
-        return nacionalidade;
-    }
+public String getNacionalidade() {
+return nacionalidade;
+}
 
-    public void setNacionalidade(String nacionalidade) {
-        this.nacionalidade = nacionalidade;
-    }
+public void setNacionalidade(String nacionalidade) {
+this.nacionalidade = nacionalidade;
+}
 
-    public String getPaisOrigem() {
-        return paisOrigem;
-    }
+public String getPaisOrigem() {
+return paisOrigem;
+}
 
-    public void setPaisOrigem(String paisOrigem) {
-        this.paisOrigem = paisOrigem;
-    }
+public void setPaisOrigem(String paisOrigem) {
+this.paisOrigem = paisOrigem;
+}
 
-    public String getEmail() {
-        return email;
-    }
+public String getEmail() {
+return email;
+}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+public void setEmail(String email) {
+this.email = email;
+}
 
-    public String getTelefone() {
-        return telefone;
-    }
+public String getTelefone() {
+return telefone;
+}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+public void setTelefone(String telefone) {
+this.telefone = telefone;
+}
 
-    public Idioma getIdioma() {
-        return idioma;
-    }
+public Idioma getIdioma() {
+return idioma;
+}
 
-    public void setIdioma(Idioma idioma) {
-        this.idioma = idioma;
-    }
+public void setIdioma(Idioma idioma) {
+this.idioma = idioma;
+}
 
-    public String getSenha() {
-        return senha;
-    }
+public String getSenha() {
+return senha;
+}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+public void setSenha(String senha) {
+this.senha = senha;
+}
 }
