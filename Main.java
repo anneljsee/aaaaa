02af5dbe5dda65a83d7fd99f252441ae762b3dd6
anneljsee <annemourag@gmail.com>
2025-10-09 16:34:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
 package classes;
 public class Main {
     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa(
-            "Lucas Silva",
-            "12/03/1998",
-            Genero.MASCULINO,
-            "Brasileira",
-            "Brasil",
-            "lucas@email.com",
-            "47 99999-8888",
-            Idioma.PORTUGUES,
-            "minhaSenha123"
-        );
+    Pessoa pessoa = new Pessoa(
+    "Lucas Silva",
+    "12/03/1998",
+    Genero.MASCULINO,
+    "Brasileira",
+    "Brasil",
+    "lucas@email.com",
+    "47 99999-8888",
+    Idioma.PORTUGUES,
+    "minhaSenha123");
     }
 }
