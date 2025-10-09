0f9690753b7af2ec3e8748ebe2d3c997e4e96b42
anneljsee <annemourag@gmail.com>
2025-10-09 20:45:13 -0300

    Update Comentario.java

--- Comentario.java
+++ Comentario.java
@@ -1,36 +1,67 @@
 package classes;
 
 public class Comentario {
-private String Autor, Data, Texto;
+private String usuario, comentario, dataComentario, horarioComentario;
+private Object like;
+private Object dislike; 
 
-public Comentario(String Autor, String Data, String Texto){
-	this.Autor = Autor;
-	this.Data = Data;
-	this.Texto = Texto;
+
+public Comentario (String usuario, String comentario, String dataComentario, String horarioComentario, Object dislike, Object like) {
+	this.usuario = usuario;
+	this.comentario = comentario;
+	this.dataComentario = dataComentario;
+	this.horarioComentario = horarioComentario;
+	this.like = like;
+	this.dislike = dislike; }
 	
-}
-public String getAutor() {
-	return Autor;
-	}
-public void setAutor(String Autor){
-	this.Autor = Autor;
-}
-public String getData() {
-	return Data;
-	}
-public void setData(String Data){
-	this.Data = Data;
-}
-public String getTexto() {
-	return Texto;
-	}
-public void setTexto(String Texto){
-	this.Texto = Texto;
-}
-
-public void ExibirComentario() {
-	System.out.println("Autor:"+Autor);
-	System.out.println("Data"+Data);
-	System.out.println("Comentário"+Texto);
-}
-}
+	public String getUsuario() {
+        return usuario;
+    }
+	
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public String getcomentario() {
+    	return comentario;
+    }
+    
+    public void setcomentario(String comentario) {
+    	this.comentario = comentario;
+    }
+    public String getdataComentario() {
+    	return dataComentario;
+    }
+    
+    public void setdataComentario(String dataComentario) {
+    	this.dataComentario = dataComentario;
+    }
+    public String gethorarioComentario() {
+    	return horarioComentario;
+    }
+    
+    public void sethorarioComentario(String horarioComentario) {
+    	this.horarioComentario = horarioComentario;
+    }	
+    public Object getlike() {
+    	return like;
+    }
+    
+    public void setlike (Object like) {
+    	this.like = like;
+    }
+    public Object getdislike() {
+    	return dislike;
+    }
+    
+    public void setdislike(Object dislike) {
+    	this.dislike = dislike;
+    }	
+    	public void exibirComentario() {
+    		System.out.println("Usuário"+usuario);
+            System.out.println("Comentário: "+comentario);
+            System.out.println("Data: "+dataComentario);
+            System.out.println("Horário: "+horarioComentario);
+            System.out.println("Likes: "+like+" Dislikes: " + dislike);
+    	
+    }
+    }
